cce44e2ed75fb994040390fdae4d50b13f52bb11
Josh Buckner <joshgbuckner@gmail.com>
2019-04-12 11:29:30 -0400

    change port for heroku

--- server.js
+++ server.js
@@ -168,8 +168,8 @@ app.delete('/upload', (req, res) => {
 
 
 
-app.listen(3000, () => {
-	console.log('api is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`api is running on ${process.env.PORT}`);
 });
 
 /* 
